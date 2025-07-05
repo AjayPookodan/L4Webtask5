63c1a40d82ebd689bb4dc43bd227fd5bae016b7b
AjayPookodan <ajaypookodan@gmail.com>
2025-07-05 22:52:47 +0530

    folder name is changed from todo-list to docs

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/docs/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
 })
+
